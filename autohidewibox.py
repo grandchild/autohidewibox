bd0ceec796b00e3b336531efa19e7b49d0522716
jakob <grandchild@gmx.net>
2020-02-26 18:30:06 +0100

    Use snake_case throughout
    Conform to PEP8 re variable + function naming.

--- autohidewibox.py
+++ autohidewibox.py
@@ -12,30 +12,30 @@ MODE_TOGGLE = "toggle"
 
 config = configparser.ConfigParser()
 try:
-    userawesomeconf = path.join(
+    user_awesome_conf = path.join(
         path.expanduser("~"), ".config/awesome/autohidewibox.conf"
     )
-    userconf = path.join(path.expanduser("~"), ".config/autohidewibox.conf")
-    systemconf = "/etc/autohidewibox.conf"
+    user_conf = path.join(path.expanduser("~"), ".config/autohidewibox.conf")
+    system_conf = "/etc/autohidewibox.conf"
     if len(sys.argv) > 1 and path.isfile(sys.argv[1]):
         config.read(sys.argv[1])
-    elif path.isfile(userawesomeconf):
-        config.read(userawesomeconf)
-    elif path.isfile(userconf):
-        config.read(userconf)
+    elif path.isfile(user_awesome_conf):
+        config.read(user_awesome_conf)
+    elif path.isfile(user_conf):
+        config.read(user_conf)
     else:
-        config.read(systemconf)
+        config.read(system_conf)
 except configparser.MissingSectionHeaderError:
     pass
 
 
-awesomeVersion = config.get("autohidewibox", "awesomeVersion", fallback=4)
-superKeys = config.get("autohidewibox", "superKeys", fallback="133,134").split(",")
+awesome_version = config.get("autohidewibox", "awesomeVersion", fallback=4)
+super_keys = config.get("autohidewibox", "superKeys", fallback="133,134").split(",")
 wiboxes = config.get("autohidewibox", "wiboxname", fallback="mywibox").split(",")
-customhide = config.get("autohidewibox", "customhide", fallback=None)
-customshow = config.get("autohidewibox", "customshow", fallback=None)
-delayShow = config.getfloat("autohidewibox", "delayShow", fallback=0)
-delayHide = config.getfloat("autohidewibox", "delayHide", fallback=0)
+custom_hide = config.get("autohidewibox", "customhide", fallback=None)
+custom_show = config.get("autohidewibox", "customshow", fallback=None)
+delay_show = config.getfloat("autohidewibox", "delayShow", fallback=0)
+delay_hide = config.getfloat("autohidewibox", "delayHide", fallback=0)
 mode = config.get("autohidewibox", "mode", fallback=MODE_TRANSIENT)
 debug = config.getboolean("autohidewibox", "debug", fallback=False)
 
@@ -43,29 +43,29 @@ debug = config.getboolean("autohidewibox", "debug", fallback=False)
 wiboxes = [w for w in wiboxes if re.match("^[a-zA-Z_][a-zA-Z0-9_]*$", w)]
 # python>=3.4: wiboxes = [ w for w in wiboxes if re.fullmatch("[a-zA-Z_][a-zA-Z0-9_]*", w) ]
 
-delay = {True: delayShow, False: delayHide}
-delayThread = None
-wiboxIsCurrentlyVisible = False
-waitingFor = False
-nonSuperKeyWasPressed = False
+delay = {True: delay_show, False: delay_hide}
+delay_thread = None
+wibox_is_currently_visible = False
+waiting_for = False
+non_super_key_was_pressed = False
 cancel = threading.Event()
 
-shPath = ""
-shPotentialPaths = ["/usr/bin/sh", "/bin/sh"]
-for p in shPotentialPaths:
+sh_path = ""
+sh_potential_paths = ["/usr/bin/sh", "/bin/sh"]
+for p in sh_potential_paths:
     if path.exists(p):
-        shPath = p
+        sh_path = p
         break
-if shPath == "":
-    print("Can't find sh in any of: " + ",".join(shPotentialPaths), file=sys.stderr)
+if sh_path == "":
+    print("Can't find sh in any of: " + ",".join(sh_potential_paths), file=sys.stderr)
     sys.exit(1)
 
-hideCommand3 = "for k,v in pairs({wibox}) do v.visible = {state} end"
-hideCommand4 = "for s in screen do s.{wibox}.visible = {state} end"
+hide_command_v3 = "for k,v in pairs({wibox}) do v.visible = {state} end"
+hide_command_v4 = "for s in screen do s.{wibox}.visible = {state} end"
 try:
-    hideCommand = hideCommand4 if int(awesomeVersion) >= 4 else hideCommand3
+    hide_command = hide_command_v4 if int(awesome_version) >= 4 else hide_command_v3
 except ValueError:
-    hideCommand = hideCommand4
+    hide_command = hide_command_v4
 
 
 def _debug(*args):
@@ -73,109 +73,109 @@ def _debug(*args):
         print(*args)
 
 
-def setWiboxState(state=True, immediate=False):
-    global delayThread, waitingFor, cancel, wiboxIsCurrentlyShown
-    wiboxIsCurrentlyShown = state
-    dbgPstate = "show" if state else "hide"
+def set_wibox_state(state=True, immediate=False):
+    global delay_thread, waiting_for, cancel, wibox_is_currently_shown
+    wibox_is_currently_shown = state
+    dbg_pstate = "show" if state else "hide"
     if delay[not state] > 0:
-        _debug(dbgPstate, "delay other")
-        if type(delayThread) == threading.Thread and delayThread.is_alive():
+        _debug(dbg_pstate, "delay other")
+        if type(delay_thread) == threading.Thread and delay_thread.is_alive():
             # two consecutive opposing events cancel out. second event should not be called
-            _debug(dbgPstate, "delay other, thread alive -> cancel")
+            _debug(dbg_pstate, "delay other, thread alive -> cancel")
             cancel.set()
             return
     if delay[state] > 0 and not immediate:
-        _debug(dbgPstate + " delay same")
-        if not (type(delayThread) == threading.Thread and delayThread.is_alive()):
-            _debug(dbgPstate, "delay same, thread dead -> start wait")
-            waitingFor = state
+        _debug(dbg_pstate + " delay same")
+        if not (type(delay_thread) == threading.Thread and delay_thread.is_alive()):
+            _debug(dbg_pstate, "delay same, thread dead -> start wait")
+            waiting_for = state
             cancel.clear()
-            delayThread = threading.Thread(
-                group=None, target=waitDelay, kwargs={"state": state}
+            delay_thread = threading.Thread(
+                group=None, target=wait_delay, kwargs={"state": state}
             )
-            delayThread.daemon = True
-            delayThread.start()
+            delay_thread.daemon = True
+            delay_thread.start()
         # a second event setting the same state is silently discarded
         return
-    _debug("state:", dbgPstate)
+    _debug("state:", dbg_pstate)
     for wibox in wiboxes:
         subprocess.call(
-            shPath
+            sh_path
             + " "
             + "-c \"echo '"
-            + hideCommand.format(wibox=wibox, state="true" if state else "false")
+            + hide_command.format(wibox=wibox, state="true" if state else "false")
             + "' | awesome-client\"",
             shell=True,
         )
 
-    customcmd = customshow if state else customhide
+    customcmd = custom_show if state else custom_hide
     if customcmd:
         subprocess.call(
-            shPath + " " + "-c \"echo '" + customcmd + "' | awesome-client\"",
+            sh_path + " " + "-c \"echo '" + customcmd + "' | awesome-client\"",
             shell=True,
         )
 
 
-def waitDelay(state=True):
+def wait_delay(state=True):
     if not cancel.wait(delay[state] / 1000):
-        setWiboxState(state=state, immediate=True)
+        set_wibox_state(state=state, immediate=True)
 
 
 try:
-    setWiboxState(False)
+    set_wibox_state(False)
 
     proc = subprocess.Popen(
         ["xinput", "--test-xi2", "--root", "3"], stdout=subprocess.PIPE
     )
 
     field = None
-    keystate = None
+    key_state = None
 
     for line in proc.stdout:
         l = line.decode("utf-8").strip()
-        eventmatch = re.match("EVENT type (\\d+) \\(.+\\)", l)
-        detailmatch = re.match("detail: (\\d+)", l)
+        event_match = re.match("EVENT type (\\d+) \\(.+\\)", l)
+        detail_match = re.match("detail: (\\d+)", l)
 
-        if eventmatch:
-            _debug(eventmatch)
+        if event_match:
+            _debug(event_match)
             try:
                 field = "event"
-                keystate = eventmatch.group(1)
+                key_state = event_match.group(1)
                 _debug("found event, waiting for detail...")
             except IndexError:
                 field = None
-                keystate = None
+                key_state = None
 
-        if (field is "event") and detailmatch:
-            _debug(detailmatch)
+        if (field is "event") and detail_match:
+            _debug(detail_match)
             try:
-                if detailmatch.group(1) in superKeys:
+                if detail_match.group(1) in super_keys:
                     _debug("is a super key")
-                    if keystate == "13":  # press
-                        nonSuperKeyWasPressed = False
+                    if key_state == "13":  # press
+                        non_super_key_was_pressed = False
                         if mode == MODE_TRANSIENT:
                             _debug("showing wibox")
-                            setWiboxState(True)
-                    if keystate == "14":  # release
+                            set_wibox_state(True)
+                    if key_state == "14":  # release
                         if mode == MODE_TRANSIENT:
                             _debug("hiding wibox")
-                            setWiboxState(False)
+                            set_wibox_state(False)
                         # Avoid toggling the wibox when a super key is used in conjunction
                         # with another key.
-                        elif mode == MODE_TOGGLE and not nonSuperKeyWasPressed:
+                        elif mode == MODE_TOGGLE and not non_super_key_was_pressed:
                             _debug("toggling wibox")
-                            setWiboxState(not wiboxIsCurrentlyShown)
-                            nonSuperKeyWasPressed = False
+                            set_wibox_state(not wibox_is_currently_shown)
+                            non_super_key_was_pressed = False
                 else:
-                    nonSuperKeyWasPressed = True
+                    non_super_key_was_pressed = True
             except IndexError:
-                _debug("Couldn't parse keystate number.")
+                _debug("Couldn't parse key_state number.")
                 pass
             finally:
                 field = None
-                keystate = None
+                key_state = None
 except KeyboardInterrupt:
     pass
 finally:
-    setWiboxState(True, True)
+    set_wibox_state(True, True)
     # print("Shutting down")
